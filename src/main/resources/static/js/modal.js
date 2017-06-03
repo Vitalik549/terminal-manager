331d7c0385bcbaea291262840153898027a8e183
Vitalik549 <vitalik549@gmail.com>
2017-06-04 02:10:48 +0300

    added jquery to libs, editable created

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -4,11 +4,13 @@ $('#modal').on('show.bs.modal', function (event) {
   $(this).find('.modal-title').text(groupName);
 
   drawModalGroupJobs(findGroupByName(groupName).group);
+ // $(this).data("group", group);
 })
 
 $('#modal').on('hidden.bs.modal', function (event) {
   $(this).find('.modal-title').text("");
   $(this).find('#modal-list').html("");
+ // $(this).removeData();
 })
 
 function drawModalGroupJobs(group){
@@ -20,20 +22,49 @@ function drawModalGroupJobs(group){
                   stop: function (e, ui) {
                       console.log('stop');
                   },
-                  //handle: '.glyphicon-align-justify',
                   cursor: "move"
               });
 
      $('#modal').find('#modal-list').append(list);
 
      if(group.jobs) group.jobs.forEach(function(job){
-        var item = $("<div class='list-group-item text-nowrap movable'/>")
-        var icon = $("<div class='pull-left glyphicon glyphicon-align-justify' />");
-        item.text(job.name);
-        item.append(icon);
 
-        list.append(item);
+        var wrap = $("<div class='list-group-item'/>")
+        var icon = $("<div class='glyphicon glyphicon-align-justify movable'/>");
+        var item = $("<div type='button' class='btn text-nowrap'/>")
+        item.click(function(){
+            drawJobForm(job);
+        });
+        item.text(job.name);
+
+        wrap.append(icon);
+        wrap.append(item);
+        list.append(wrap);
      });
 
      list.append('<button type="button" class="list-group-item btn btn-sm btn-block"><span class="glyphicon glyphicon-plus-sign"></span></button>')
 }
+
+function drawJobForm(job){
+    console.log(job);
+    $('#editor').html("");
+
+    var wrapper = $("<div class='container-fluid'/>");
+
+    var ename = $('<a >test</a>');;
+
+    wrapper.append(ename);
+    $('#editor').append(wrapper);
+
+}
+
+/*
+
+  "name": "test job",
+  "description": "starting tests",
+  "command" : "mvn --help",
+  "startingDirectory" : "/Users/admin/work/projects/crud-tests",
+  "baseLogFile" : "/Users/admin/Desktop/ZZZZZ.txt",
+  "logStrategy" : "append"
+
+*/
